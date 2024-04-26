3cce552db2c0b20626c37b63a8ce17a9fa604f1c
Nathan Stader <nathan.stader@heig-vd.ch>
2024-04-26 11:35:32 +0200

    Finition de la map deck.gl

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 
 import { drawMultilineChart } from './js/d3.js';
-import  { createHeatmap } from './js/heatmap';
+import './js/heatmap';
 import data from './data/avalanches.json'
 import "./js/locomotiveScroll.js"
 
 drawMultilineChart(data);
-console.warn(data);
 let pourcentage = document.querySelectorAll('#sec5 > div > div')
 pourcentage.forEach(function(div) {
     div.addEventListener('mouseenter', (e)=>{
         if (!e.target.classList.contains('active')) {
-            document.querySelector('.active').classList.remove('active')        
-            e.target.classList.add("active");
+            document.querySelector('.active').classList.remove('active')      
+            setTimeout(() => {
+                e.target.classList.add("active");
+              }, 500);
         }
     });
 });
@@ -25,10 +26,3 @@ function calculerHauteurTotale() {
     
     return hauteurTotale;
   }
-
-
-// Création de la heatmap et ajout à la section 3
-const containerId = document.getElementById('map');
-createHeatmap(containerId);
-console.warn(map);
-
